d3d9dbbfad4ddadd12eef96deca172860ab88459
Beauchamp MEHDI <mehdi.b02@hotmail.fr>
2023-02-13 11:43:31 +0100

    Add scritp SceneHandler

---
+++ Assets/Script/SceneHandler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
